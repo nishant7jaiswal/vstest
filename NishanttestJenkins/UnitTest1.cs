4915cfe294ea7bbaa7cdfe23829f47a235230d0b
Nishant Jaiswal <nishant.jaiswal@Perficient.com>
2019-01-30 14:34:09 +0530

    Add project files.

---
+++ NishanttestJenkins/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NishanttestJenkins
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            int a = 5;
+
+            System.Console.WriteLine("This is test -"+a);
+
+
+
+        }
+    }
+}
